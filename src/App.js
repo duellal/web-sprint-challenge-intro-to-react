1f1eda4f42e5ba7a9e03d24e1fc6f58153625e51
duellal <alexandriaduell@gmail.com>
2022-07-20 14:55:46 -0400

    Adding a drop down details section to each character

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import Character from '../src/components/Character'
+import Details from '../src/components/Details'
 
 const starWarsEndpoint = `https://swapi.dev/api/people/`
 
@@ -8,10 +9,15 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([])
+  const [currentCharId, setCurrentCharId] = useState(null)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const closeDetails = () => {
+    setCurrentCharId(null)
+  }
 
   useEffect(() => {
     axios
@@ -34,6 +40,9 @@ const App = () => {
         characters.map((person, i) => {
           return <Character key={person[i]} charInfo={person} />
         })
+      }
+      {
+        currentCharId && <Details charId={currentCharId} close={closeDetails} />
       }
     </div>
   );
---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React, { } from 'react'
+
+const Details = props => {
+   const { charId, closeDetails } = props
+
+   return (
+      
+   )
+}
+
+export default Details
