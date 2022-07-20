ebc1065b2d0373d98468f86caaef12794c98a21f
duellal <alexandriaduell@gmail.com>
2022-07-20 14:49:10 -0400

    Added Characters to the DOM + they are showing on the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import Character from '../src/components/Character'
+
+const starWarsEndpoint = `https://swapi.dev/api/people/`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get(starWarsEndpoint)
+      .then(res => {
+        setCharacters(res.data)
+        console.log(res.data)
+      }
+      )
+      .catch(err => {
+        console.log(err)
+      })
+  }, []
+  )
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map((person, i) => {
+          return <Character key={person[i]} charInfo={person} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React, { } from 'react'
+
+const Character = props => {
+   const { charInfo } = props
+
+   return (
+      <div>
+         {charInfo.name}
+      </div>
+   )
+}
+
+export default Character
